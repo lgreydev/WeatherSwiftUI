2b9f1165e496ed6cb30af8a39f7bd9cdb0ce53f2
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-15 21:32:34 +0200

    Created a new project

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Sergey Lukaschuk on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
