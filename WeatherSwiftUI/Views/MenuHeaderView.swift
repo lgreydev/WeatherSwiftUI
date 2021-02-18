8767eebc1e3d384a5f1364a59630b81b280f1c53
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-02-18 10:17:52 +0200

    Added Menu Header

---
+++ WeatherSwiftUI/Views/MenuHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  MenuHeaderView.swift
+//  WeatherSwiftUI
+//
+//  Created by Sergey Lukaschuk on 18.02.2021.
+//
+
+import SwiftUI
+
+struct MenuHeaderView: View {
+    
+    @ObservedObject var cityVM: CityViewModel
+    @State private var searchTerm = "Zaporizhzhia"
+    
+    var body: some View {
+       
+        HStack {
+            TextField("", text: $searchTerm)
+                .padding(.leading, 20.0)
+            
+            Button { cityVM.city = searchTerm } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color.blue)
+                    
+                    Image(systemName: "location.fill")
+                }
+            }
+            .frame(width: 50.0, height: 50.0)
+        }
+        .foregroundColor(.white)
+        .padding()
+        .background(ZStack(alignment: .leading) {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.white)
+                .padding(.leading, 10.0)
+            
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill(Color.blue.opacity(0.5))
+        })
+    }
+}
+
+struct MenuHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
